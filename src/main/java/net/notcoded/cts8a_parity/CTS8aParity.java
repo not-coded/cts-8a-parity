d0bfa62ca9b3f5f5b062a9ba22196b3b145efdb3
Alexandra-Myers <bia416cat@gmail.com>
2024-08-01 15:13:09 -0400

    implement receiver so servers can disable bridging

--- src/main/java/net/notcoded/cts8a_parity/CTS8aParity.java
+++ src/main/java/net/notcoded/cts8a_parity/CTS8aParity.java
@@ -1,9 +1,15 @@
 package net.notcoded.cts8a_parity;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.minecraft.resources.ResourceLocation;
 
 public class CTS8aParity implements ClientModInitializer {
+	public static boolean enabledBridging = true;
 	@Override
 	public void onInitializeClient() {
+		ClientPlayNetworking.registerGlobalReceiver(new ResourceLocation("c", "update_status"), (client, handler, buf, responseSender) -> enabledBridging = buf.readBoolean());
+		ClientPlayConnectionEvents.DISCONNECT.register((handler, client) -> enabledBridging = true);
 	}
 }
--- src/main/java/net/notcoded/cts8a_parity/mixin/GameRendererMixin.java
+++ src/main/java/net/notcoded/cts8a_parity/mixin/GameRendererMixin.java
@@ -8,6 +8,7 @@ import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
+import net.notcoded.cts8a_parity.CTS8aParity;
 import net.notcoded.cts8a_parity.extensions.BlockHitResultExtensions;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -24,7 +25,7 @@ public class GameRendererMixin {
     @Inject(method = "pick", at = @At(value = "FIELD", target = "Lnet/minecraft/client/Minecraft;hitResult:Lnet/minecraft/world/phys/HitResult;", shift = At.Shift.AFTER, ordinal = 2), locals = LocalCapture.CAPTURE_FAILEXCEPTION)
     public void bedrockBridging(float f, CallbackInfo ci, Entity entity, float g, Vec3 vec3, Vec3 vec32, Vec3 vec33, float h, AABB aABB, EntityHitResult entityHitResult, boolean bl, double d, HitResult hitResult) {
         // START NEW CODE (8a)
-        if (entity.isOnGround() && vec3.y < -0.7) {
+        if (CTS8aParity.enabledBridging && entity.isOnGround() && vec3.y < -0.7) {
             Vec3 dcm15 = entity.getPosition(f).add(0.0, -0.1, 0.0);
             Vec3 dcm16 = dcm15.add(vec3.x, 0.0, vec3.z);
             hitResult = entity.level.clip(new ClipContext(dcm16, dcm15, ClipContext.Block.COLLIDER, ClipContext.Fluid.NONE, entity));
