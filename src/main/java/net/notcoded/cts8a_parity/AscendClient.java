cb5ebc5a38ad8cb35a65665e5502e26abc65ddca
betterclient <iamanaltlolwhypog@gmail.com>
2024-11-03 15:31:22 +0300

    add config

--- src/main/java/net/notcoded/cts8a_parity/AscendClient.java
+++ src/main/java/net/notcoded/cts8a_parity/AscendClient.java
@@ -2,12 +2,22 @@ package net.notcoded.cts8a_parity;
 
 import io.github.betterclient.acl.CustomModBuilder;
 
+import java.io.File;
+import java.io.IOException;
+
 public class AscendClient {
     public void onAscend() {
         CustomModBuilder.builder()
                 .setModName("Bedrock Bridge")
-                .setLoadFunction(() -> false)
-                .setSaveFunction(empty -> {})
+                .setLoadFunction(() -> new File("BedrockBridgeState.txt").exists())
+                .setSaveFunction(state -> {
+                    File f = new File("BedrockBridgeState.txt");
+                    try {
+                        boolean unused = state ? f.createNewFile() : f.delete();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
                 .setCustomToggle(aBoolean -> {
                     if (CTS8aParity.bridgingAllowed) {
                         CTS8aParity.enabledBridging = aBoolean;
